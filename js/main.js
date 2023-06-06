274fe2e14b9d6d9baff2e93003ea3285d3399139
hardiksuthar74 <hardiksuthar74@gmail.com>
2023-06-06 10:58:56 +0530

    final

--- js/main.js
+++ js/main.js
@@ -4,51 +4,51 @@
 const getComputerChoice = () => {
   const moves = ["rock", "paper", "scissors"];
   const move = Math.floor(Math.random() * 3);
-
   return moves[move];
 };
 
 // game round function
 
-const playRound = (playerSelection, computerSelection) => {
+const userHand = document.querySelectorAll(".btn");
+const win = document.querySelector(".win");
+const lose = document.querySelector(".lose");
+const tie = document.querySelector(".tie");
+const reset = document.querySelector(".resets");
+
+win.textContent = 0;
+lose.textContent = 0;
+tie.textContent = 0;
+
+userHand.forEach((hand) => {
+  hand.addEventListener("click", () => {
+    let userMove = hand.value;
+    let cpuMove = getComputerChoice();
+    game(userMove, cpuMove);
+  });
+});
+
+const game = (playerSelection, computerSelection) => {
   if (playerSelection === computerSelection) {
-    return "Its a Tie";
-  } else if ((playerSelection === "rock") & (computerSelection === "paper")) {
-    return "You Lose";
+    return (tie.textContent = tie.textContent * 1 + 1);
   } else if (
-    (playerSelection === "rock") &
-    (computerSelection === "scissors")
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
   ) {
-    return "You Won";
-  } else if (
-    (playerSelection === "paper") &
-    (computerSelection === "scissors")
-  ) {
-    return "You Lose";
-  } else if ((playerSelection === "paper") & (computerSelection === "rock")) {
-    return "You Won";
-  } else if (
-    (playerSelection === "scissors") &
-    (computerSelection === "paper")
-  ) {
-    return "You Won";
-  } else if (
-    (playerSelection === "scissors") &
-    (computerSelection === "rock")
-  ) {
-    return "You Lose";
+    return (win.textContent = win.textContent * 1 + 1);
+  } else {
+    return (lose.textContent = lose.textContent * 1 + 1);
   }
 };
 
-const game = () => {
-  let i = 0;
-  while (i < 5) {
-    const userMove = prompt("Your move: ");
-    const cpuMove = getComputerChoice();
-    const result = playRound(userMove, cpuMove);
-    console.log(result);
-    i++;
-  }
-};
+reset.addEventListener("click", () => {
+  win.textContent = 0;
+  lose.textContent = 0;
+  tie.textContent = 0;
 
-// game();
+  reset.classList.add("dance-animation");
+
+  setTimeout(function () {
+    reset.classList.remove("dance-animation");
+  }, 1000);
+});
